31ab90fabc28db8c51ba49d86fcb093a3a473245
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-01 22:04:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,46 +1,25 @@
-class School
+class school
+  attr_accessor :name, :roster
 
-  attr_accessor :school
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
 
-  def initialize(roster)
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(student_name, grade)
-<<<<<<< HEAD
-    if roster.has_key?(grade)
-      roster[grade] << student_name
-    else
-      roster[grade] = []
+    def add_student(student_name, grade)
+      roster[grade] ||= []
       roster[grade] << student_name
     end
-      roster
-  end
 
-  def grade(num)
-    if roster.has_key?(num)
-      roster[num]
+    def grade(student_grade)
+      roster[student_grade]
+    end
+
+    def sort
+      sorted = {}
+      roster.each do |grade, students|
+        sorted[grade] = students.sort
+      end
+      sorted
     end
   end
-
-  def sort
-    sorted_hash = {}
-      roster.each do |key, value|
-        sorted_hash[key] = value.sort
-    end
-    sorted_hash
-  end
-=======
-    if grade != nil
-      roster[grade] << student_name
-    end
-  end
-
-
-
->>>>>>> 88d51ed243a9d2b42fbcacb81088399b741bd8c7
-end
