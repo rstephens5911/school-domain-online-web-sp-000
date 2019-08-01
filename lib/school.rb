6f58e5a406449a37269d84e55c7948096423f63c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-01 22:01:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,6 +11,7 @@ class School
   end
 
   def add_student(student_name, grade)
+<<<<<<< HEAD
     if roster.has_key?(grade)
       roster[grade] << student_name
     else
@@ -33,4 +34,13 @@ class School
     end
     sorted_hash
   end
+=======
+    if grade != nil
+      roster[grade] << student_name
+    end
+  end
+
+
+
+>>>>>>> 88d51ed243a9d2b42fbcacb81088399b741bd8c7
 end
